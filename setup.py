a27fda652f295088454a60fca632e811b80d9433
Adi Eyal <adi@openup.org.za>
2020-05-22 16:30:56 +0200

    Updated setup.py details

--- setup.py
+++ setup.py
@@ -4,20 +4,19 @@ with open("README.md", encoding='utf-8') as fp:
     long_description = fp.read()
 
 setup(
-    name = "icon_picker_widget",
-    version = "0.1",
+    name="Django Material Icon Widget",
+    version="0.1",
     license="MIT",
-    description = "Icon Picker for admin panel In Django",
+    description="Icon Picker for admin panel In Django",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    author = "Adi Eyal",
-    author_email = "adi@openup.org.za",
-    url = "https://github.com/adieyal/dictutils", #<- should be changed
-    download_url = "https://github.com/adieyal/dictutils/archive/v0.1.tar.gz",  #<- should be changed 
+    author="Adi Eyal",
+    author_email="adi@openup.org.za",
+    url="https://github.com/adieyal/django-material-icon-widget",
+    download_url="https://github.com/adieyal/dictutils/archive/v0.1.tar.gz",  #<- should be changed 
     packages=find_packages(),
-    keywords = ["widget", "utility"],
-    #install_requires = [],
-    classifiers = [
+    keywords=["widget", "utility"],
+    classifiers=[
         "Development Status :: 4 - Beta",
         "Intended Audience :: Developers",
         "Topic :: Software Development :: Build Tools",
